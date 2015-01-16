e2d1940a987b503b51ad9651847b8e3455112b0f
vikraant <vsinghsanwal@gmail.com>
2015-01-16 12:51:16 +0530

    Part of prev commit itself, not commited by mistake

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame, os, sys
 from pygame.locals import *
 
 from players import Ghosts, PacMan
+from wallsntokens import Walls
 
 """Global constants"""
 """Creating Basic colors"""
@@ -27,6 +28,8 @@ class Game:
         self.token_list = pygame.sprite.Group()
         """This is a list of all the ghost sprites"""
         self.ghost_list = pygame.sprite.Group()
+        """This is a list of all the walls"""
+        self.wall_list = pygame.sprite.Group()
         """This is a list of all the sprites created in the entire game"""
         self.all_sprites_list = pygame.sprite.Group()
 
@@ -42,14 +45,22 @@ class Game:
             self.ghosts[color].rect.y = 350
             """Adding Ghost to the required sprite groups"""
             self.ghost_list.add(self.ghosts[color])
-            self.all_sprites_list.add(self.ghosts[color])
+            #self.all_sprites_list.add(self.ghosts[color])
             x+=40
         """PacMan Sprite"""
-        self.pacman = PacMan("pacman.png")
+        self.pacman = PacMan("pacmanleft.png")
         """Assigning start position to PacMan"""
         self.pacman.rect.x = 390
         self.pacman.rect.y = 475
-        self.all_sprites_list.add(self.pacman)    
+        self.all_sprites_list.add(self.pacman)
+        """Wall Sprites"""
+        """List of all the top, left, width and height of all the walls to be created"""
+        wall_xywh = [(37, 36, 228, 1), (266, 37, 1, 95), (265, 131, 12, 1), (276, 37, 1, 95), (277, 36, 228, 1), (36, 37, 1, 205), (506, 37, 1, 205), (37, 243, 95, 1), (409, 243, 95, 1), (131, 242, 1, 83), (410, 242, 1, 80), (27, 324, 105, 1), (409, 321, 105, 1), (27, 363, 105, 1), (409, 366, 105, 1), (131, 362, 1, 83), (410, 366, 1, 83), (37, 445, 95, 1), (409, 445, 95, 1), (36, 446, 1, 83), (506, 446, 1, 83), (37, 530, 47, 1), (457, 530, 47, 1), (83, 529, 1, 37), (458, 529, 1, 37), (37, 565, 47, 1), (457, 565, 47, 1), (36, 566, 1, 84), (506, 566, 1, 84), (37, 651, 468, 1)]
+        for xywh in wall_xywh:
+            wall = Walls(xywh[0], xywh[1], xywh[2], xywh[3])
+            self.wall_list.add(wall)
+            self.all_sprites_list.add(wall)
+        self.pacman.walls = self.wall_list
 
     def manageEvents(self):
         """Checking all the events occured in this frame"""
@@ -73,10 +84,10 @@ class Game:
         if not self.game_over:
             self.all_sprites_list.update()
         
-    def displayFrame(self, DISPSURFACE):
+    def displayFrame(self, DISPSURFACE, background):
         "Renders the changes to the main display"""
-        """Clear the screen"""
-        DISPSURFACE.fill(WHITE)
+        """Clear the screen and render background"""
+        DISPSURFACE.blit(background, (20, 20))
 
         """if self.game_over:
             font = pygame.font.SysFont("serif", 25)
@@ -99,7 +110,47 @@ def main():
     """Creating the Main display surface"""
     DISPSURFACE = pygame.display.set_mode((780, 700),0, 32)
     pygame.display.set_caption('PacMan')
-  
+
+    """Creating the background image"""
+    background_to_load = os.path.join('Data', 'Images', 'pacback.jpg')
+    background = pygame.image.load(background_to_load).convert()
+    """Rendering the background to the display"""
+    DISPSURFACE.blit(background, (20, 20))
+
+    #test pixels for wall pixels
+    pixObj = pygame.PixelArray(DISPSURFACE)
+    pixObj[20][20] = WHITE #Start pt. of image
+    pixObj[37][37] = WHITE #top left  1
+    pixObj[265][37] = WHITE #top center left   2
+    pixObj[277][37] = WHITE #top center right   3
+    pixObj[265][132] = WHITE #top center down left    27
+    pixObj[277][132] = WHITE #top center down right   28
+    pixObj[505][37] = WHITE #top right   4
+    pixObj[37][242] = WHITE #mid left top    5
+    pixObj[505][242] = WHITE #mid right top   6
+    pixObj[132][242] = WHITE #mid left top right    11
+    pixObj[409][242] = WHITE #mid right top left    12
+    pixObj[132][325] = WHITE #mid left mid top right    13
+    pixObj[409][325] = WHITE #mid right mid top left    14
+    pixObj[37][446] = WHITE #mid left bottom    7
+    pixObj[505][446] = WHITE #mid right bottom    8
+    pixObj[132][446] = WHITE #mid left bottom right    17
+    pixObj[409][446] = WHITE #mid right bottom left     18
+    pixObj[132][362] = WHITE #mid left mid bottom right    15
+    pixObj[409][362] = WHITE #mid right mid bottom left    16
+    pixObj[37][529] = WHITE #down left top   23
+    pixObj[505][529] = WHITE #down right top   24
+    pixObj[84][529] = WHITE #down left top right    19   
+    pixObj[457][529] = WHITE #down right top left   20
+    pixObj[37][566] = WHITE #down left bottom   25
+    pixObj[505][566] = WHITE #down right bottom   26
+    pixObj[84][566] = WHITE #down left bottom right    21
+    pixObj[457][566] = WHITE #down right bottom left   22
+    pixObj[37][650] = WHITE #bottom left    9
+    pixObj[505][650] = WHITE #bottom right   10
+    del pixObj
+    #
+
     """Creating a Game Instance"""
     game = Game()
     """Creating the FPS Clock"""
@@ -111,10 +162,10 @@ def main():
         """calling processing function to process the events and update objects"""
         game.runLogic()
         """Calling render function to render all the drawn changes to the display"""
-        game.displayFrame(DISPSURFACE)
+        game.displayFrame(DISPSURFACE, background)
 
         """Limit to 60 frames per second"""
-        fps_clock.tick(60)
+        fps_clock.tick(30)
 
 if __name__ == '__main__':
     main()
--- players.py
+++ players.py
@@ -10,6 +10,8 @@ class Ghosts(pygame.sprite.Sprite):
         image_to_load = os.path.join('Data', 'Images', name)
         self.image = pygame.image.load(image_to_load).convert()
         self.rect = self.image.get_rect()
+
+        self.walls = None
     """make 3 update fns (1 named update which then calls the rest so tht v can do self.all_sprites_list.update()
         - 1 for AI i.e. they keep roaming, second to go behind PacMan,
         3rd when Pacdies or they die and hv to go back inside box"""
@@ -26,6 +28,7 @@ class PacMan(pygame.sprite.Sprite):
 
         """Current Direction of PacMan based on keyboard input"""
         self.direction = None
+        self.walls = None
 
     def update(self):
         """Update PacMan's location based on Gamer Input"""
@@ -38,13 +41,6 @@ class PacMan(pygame.sprite.Sprite):
             if self.rect.right >= 780:
                 self.rect.right = 780
 
-        elif self.direction == 'down':
-            image_to_load = os.path.join('Data', 'Images', 'pacmandown.png')
-            self.image = pygame.image.load(image_to_load).convert()
-            self.rect.y += 5
-            if self.rect.bottom >= 700:
-                self.rect.bottom = 700
-
         elif self.direction == 'left':
             image_to_load = os.path.join('Data', 'Images', 'pacmanleft.png')
             self.image = pygame.image.load(image_to_load).convert()
@@ -52,10 +48,33 @@ class PacMan(pygame.sprite.Sprite):
             if self.rect.left <= 0:
                 self.rect.left = 0
 
+        elif self.direction == 'down':
+            image_to_load = os.path.join('Data', 'Images', 'pacmandown.png')
+            self.image = pygame.image.load(image_to_load).convert()
+            self.rect.y += 5
+            if self.rect.bottom >= 700:
+                self.rect.bottom = 700
+
         elif self.direction == 'up':
             image_to_load = os.path.join('Data', 'Images', 'pacmanup.png')
             self.image = pygame.image.load(image_to_load).convert()
             self.rect.y -= 5
             if self.rect.top <= 0:
                 self.rect.top = 0
+
+        """Did this update cause us to hit a wall"""
+        """make a list of all collissions between PacMan and the walls"""
+        block_hit_list = pygame.sprite.spritecollide(self, self.walls, False)
+        for block in block_hit_list:
+            # If we are moving right, set our right side to the left side of the item we hit
+            if self.direction == 'right':
+                self.rect.right = block.rect.left
+            elif self.direction == 'left':
+                # Otherwise if we are moving left, do the opposite.
+                self.rect.left = block.rect.right
+            # Reset our position based on the top/bottom of the object.
+            elif self.direction == 'down':
+                self.rect.bottom = block.rect.top
+            elif self.direction == 'up':
+                self.rect.top = block.rect.bottom
 
